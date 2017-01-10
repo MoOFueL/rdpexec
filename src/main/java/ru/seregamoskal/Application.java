09d89f4fdd31d15743f86d807db954ed39b5a466
Иванов Дмитрий Владимирович <dmvlivanov@at-consulting.ru>
2017-01-10 16:36:31 +0300

    initial commit

---
+++ src/main/java/ru/seregamoskal/Application.java
@@ -0,0 +1,16 @@
+package ru.seregamoskal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Dmitriy
+ */
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
