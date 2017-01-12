ae0fe4c8b4cda410dc09757ba57e5ff178a9ffe8
thezekes <thezeke23@gmail.com>
2017-01-12 14:15:49 +0300

    Created new method "available" which use java.net.socket library. This method check available of port+IP and return true or false.

--- src/main/java/ru/seregamoskal/rdpExec/services/TelNetService.java
+++ src/main/java/ru/seregamoskal/rdpExec/services/TelNetService.java
@@ -2,9 +2,21 @@ package ru.seregamoskal.rdpExec.services;
 
 import org.springframework.stereotype.Service;
 
+import java.io.IOException;
+import java.net.Socket;
+
 /**
  * Created by Дмитрий on 11.01.2017.
  */
 @Service
 public class TelNetService {
+    private static boolean available(String ip, int port) {
+        try (Socket ignored = new Socket(ip ,port ))
+        {
+            return false;
+        }
+        catch (IOException ignored) {
+            return true;
+        }
+    }
 }
