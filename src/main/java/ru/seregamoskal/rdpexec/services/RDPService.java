4fc893674121a2f14791a240d20470f559e50eec
Дмитрий <dmvlivanov@at-consulting.ru>
2017-01-20 00:24:25 +0300

    refactoring

--- src/main/java/ru/seregamoskal/rdpexec/services/RDPService.java
+++ src/main/java/ru/seregamoskal/rdpexec/services/RDPService.java
@@ -11,8 +11,7 @@ import ru.seregamoskal.rdpexec.domain.ServerInfo;
 
 import java.io.IOException;
 import java.nio.charset.Charset;
-import java.util.Date;
-import java.util.List;
+import java.util.*;
 
 /**
  * Created by Дмитрий on 11.01.2017.
@@ -24,6 +23,7 @@ public class RDPService {
     private LoginInfoService loginInfoService;
 
     private static final String SPACE = " ";
+    private static final String DOUBLE_AMPERSAND = "&&";
 
     @Autowired
     public void setServerInfoService(ServerInfoService serverInfoService) {
@@ -53,16 +53,35 @@ public class RDPService {
     // TODO: 16.01.2017 реализовать описанный выше метод здесь
 
     /**
-     * @param commandsAndArguments - список который содержит в себе команды
-     *                             и их аргументы в правильном порядке
+     * @param commandsAndArgumentsMap - мапа, где ключом является команда к исполнению,
+     *                                а значением - список аргументов этой команды
      * @return - {@link Operation} с данными об операции
      */
-    private Operation executeOperation(List<String> commandsAndArguments) throws IOException, InterruptedException {
+    private Operation executeOperation(Map<String, List<String>> commandsAndArgumentsMap) throws IOException, InterruptedException {
 
         final ProcessBuilder processBuilder = new ProcessBuilder();
-        processBuilder.command(commandsAndArguments);
+        List<String> commandsAndArgumentsList = transformToListOfStrings(commandsAndArgumentsMap);
+        processBuilder.command(commandsAndArgumentsList);
         final Process process = processBuilder.start();
-        return makeResult(commandsAndArguments, process);
+        return makeResult(commandsAndArgumentsList, process);
+    }
+
+    private List<String> transformToListOfStrings(Map<String, List<String>> commandsAndArguments) {
+
+        final List<String> result = new ArrayList<>();
+        final Iterator<Map.Entry<String, List<String>>> iterator = commandsAndArguments.entrySet().iterator();
+
+        while (iterator.hasNext()) {
+            final Map.Entry<String, List<String>> entry = iterator.next();
+            final String command = entry.getKey();
+            final List<String> arguments = entry.getValue();
+            result.add(command);
+            result.addAll(arguments);
+            if (iterator.hasNext()) {
+                result.add(DOUBLE_AMPERSAND);
+            }
+        }
+        return result;
     }
 
     private Operation makeResult(List<String> commandsAndArguments, Process process) throws IOException, InterruptedException {
